ab6cfabcf9eff84a3cced423dfa8b8565362ad11
Raj Sekhar Debnath <rajsekhar.debnath@tcs.com>
2018-05-25 11:22:26 +0300

    use coroutines to direct output

--- tail.py
+++ tail.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 def tail(file):
 	file.seek(0, 2) #go to end of file
@@ -10,3 +11,35 @@ def tail(file):
 		yield line
 		
 #works like tail -f
+with open(sys.argv[1]) as file:
+	for line in tail(file):
+		print line
+		
+#_______________________________________________________________
+#we want to send the line to another output like a printer. we write a coroutine to consume each line and send it to a supposed printer
+def coroutine(fn):
+	def execute(*args, **kwargs):
+		res = fn(*args, **kwargs)
+		next(res)
+		return res
+	return execute
+	
+@coroutine
+def printer():
+	while True:
+		line = yield
+		print line #supposedly prints to a printer
+
+def tail_target(file, target):
+	file.seek(0, 2)
+	while True:
+		line = file.readline()
+		if not line:
+			time.sleep(0.4)
+			continue
+		target.send(line)
+		
+
+#tail -f > printer
+with open(sys.argv[1]) as file:
+	tail_target(file, printer())
